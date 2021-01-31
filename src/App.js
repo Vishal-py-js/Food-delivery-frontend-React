e21f8c047149f3e71c2a5d0195cafca3d03e71ed
Vishal Kumar <vskkr007@gmail.com>
2021-01-31 20:36:11 +0530

    Added user login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Store from './Store'
 import Cart from './Cart'
+import SignUp from './SignUp'
 import Login from './Login'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
 function App() {
   return (
       <Router>
         <Switch>
+                <Route path="/signup" exact component={SignUp} />
                 <Route path="/login" exact component={Login} />
                 <Route path="/" exact component={Store} />
                 <Route path="/cart/" exact component={Cart} />
