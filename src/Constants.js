1b5ea3d2520e8e4864e0b902016909d01b278bd5
Vishal Kumar <vskkr007@gmail.com>
2021-02-09 20:20:18 +0530

    changed BaseURL in Contants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,7 @@
 // change this url to the url where the backend is deployed
 // Also change the url manually in the CategorizedItem.js at Line 9
 
-const BaseURL = 'http://127.0.0.1:8000/api/'
+// const BaseURL = 'http://127.0.0.1:8000/api/'
+const BaseURL = 'https://fullstackecom.herokuapp.com/api/'
 
 export default BaseURL
