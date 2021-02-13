2b8c1ba5cb59f5e7c4cc4fa2dd47ca1aba86f088
Vishal Kumar <vskkr007@gmail.com>
2021-02-13 20:49:02 +0530

    added setInterval in App.js to prevent backend dynos to go to sleep

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ import Login from './Login'
 import Sidebar from './Sidebar'
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import axios from 'axios';
+import BaseURL from './Constants';
 
 
 function App() {
- 
+  
+  setInterval(() => {
+    axios.get(BaseURL)
+  }, 3000000);
+
   return (
       <React.Fragment>
       <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'}/>
