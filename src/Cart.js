a1e8d7a605831a3cfdf0f9544f96af40e7799c20
Vishal Kumar <vskkr007@gmail.com>
2021-02-14 21:50:37 +0530

    added 'Rs' sign in cart.js

--- src/Cart.js
+++ src/Cart.js
@@ -80,7 +80,7 @@ function Cart() {
                         <thead>
                             <tr>
                                 <th><h5>Items: <strong>{localStorage.getItem('item-count')}</strong></h5></th>
-                                <th><h5>Total: <strong>{sumtotal}</strong></h5></th>
+                                <th><h5>Total: <strong>Rs {sumtotal}</strong></h5></th>
                                 <a style={{float: 'right', margin: '5px'}} className="btn btn-success" href="#">Checkout</a>
                             </tr>
                         </thead>
