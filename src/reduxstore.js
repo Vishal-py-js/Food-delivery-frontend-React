ea035ef4ceb2823ecaef947186dfde24f15c76eb
Vishal Kumar <vskkr007@gmail.com>
2021-02-28 18:34:34 +0530

    removed logger

--- src/reduxstore.js
+++ src/reduxstore.js
@@ -1,7 +1,6 @@
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger'
 import itemsReducer from './redux/ActionReducer'
 
 const store = createStore(itemsReducer, composeWithDevTools(applyMiddleware(thunk)))
