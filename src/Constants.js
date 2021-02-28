4d8dee5e50f986a394026907779a03b59268ea1c
Vishal Kumar <vskkr007@gmail.com>
2021-02-28 18:45:00 +0530

    changed constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,4 @@
 // change this url to the url where the backend is deployed
-// Also change the url manually in the CategorizedItem.js at Line 9
 
 // const BaseURL = 'http://127.0.0.1:8000/api/'
 const BaseURL = 'https://fullstackecom.herokuapp.com/api/'
