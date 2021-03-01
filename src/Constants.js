5dfb1e1f6eb4c8076c4f4085fc9b3ad13a23c021
Vishal Kumar <vskkr007@gmail.com>
2021-03-01 22:50:07 +0530

    made changes to Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,6 @@
 // change this url to the url where the backend is deployed
 
+// const BaseURL = 'http://127.0.0.1:8000/api/'
 const BaseURL = 'https://fullstackecom.herokuapp.com/api/'
 
 export default BaseURL
